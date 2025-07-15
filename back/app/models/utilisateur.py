ddff11ea1e890bf1659f7a6a9bb864b9e1438289
sarobidy-dev <rabemalalasarobidyravakiniaina@gmail.com>
2025-07-15 11:23:29 +0300

    route

--- back/app/models/utilisateur.py
+++ back/app/models/utilisateur.py
@@ -7,7 +7,7 @@ class Utilisateur(Base):
     id = Column(Integer, primary_key=True, index=True)
     nom = Column(String(120), nullable=False)
     email = Column(String(120), unique=True, index=True, nullable=False)
-    mot2pass = Column(String(120), unique=True, index=True, nullable=False)
+    mot2pass = Column(String(120),nullable=False)
     role = Column(String(40), nullable=False)
     actif= Column(Boolean, default=True)
     photo_profil = Column(String(255), nullable=True)
